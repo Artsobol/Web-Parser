c2a00843c79ec1ea44b0b37950853b428806cc75
Karina534 <smirnova7.04@mail.ru>
2025-03-05 16:24:58 +0300

    added fields page with code

--- src/main/java/com/example/test/controller/FieldController.java
+++ src/main/java/com/example/test/controller/FieldController.java
@@ -1,6 +1,9 @@
 package com.example.test.controller;
 
+import com.example.test.dto.CodeDto;
+import com.example.test.dto.FieldAndCodeDto;
 import com.example.test.dto.FieldDto;
+import com.example.test.service.CodeService;
 import com.example.test.service.FieldService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
@@ -8,26 +11,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@AllArgsConstructor
-//@RestController
-//@RequestMapping("/fields")
-//public class FieldController {
-//    private FieldService fieldService;
-//
-//    @Operation(summary = "Create field if study",
-//            description = "Take DTO, save it in bd and return saved object.")
-//    @PostMapping
-//    public ResponseEntity<FieldDto> createField(@RequestBody FieldDto fieldDto){
-//        FieldDto fieldDto1 = fieldService.create(fieldDto);
-//        return new ResponseEntity<>(fieldDto1, HttpStatus.CREATED);
-//    }
-//}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/fields")
 public class FieldController {
     private FieldService fieldService;
+    private CodeService codeService;
+
+    @Operation(summary = "Get all fields with code")
+    @GetMapping
+    public ResponseEntity<List<FieldAndCodeDto>> getAllFieldsWithCode(){
+        List<FieldDto> fieldDtos = fieldService.getAll();
+        List<CodeDto> codeDtos = codeService.getAll();
+        Map<Long, CodeDto> codeDtoMap = codeDtos.stream()
+                .collect(Collectors.toMap(CodeDto::getId, codeDto -> codeDto));
+
+        List<FieldAndCodeDto> fieldAndCodeDtos = new ArrayList<>();
+        for (FieldDto fieldDto: fieldDtos){
+            Long codeId = fieldDto.getCode();
+            CodeDto codeDto = codeDtoMap.get(codeId);
+            fieldAndCodeDtos.add(new FieldAndCodeDto(fieldDto, codeDto));
+        }
+
+        return ResponseEntity.ok(fieldAndCodeDtos);
+    }
 
     @Operation(summary = "Create field if study",
             description = "Take DTO, save it in bd and return saved object.")
